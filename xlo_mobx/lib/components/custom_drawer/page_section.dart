1a3249a32b2934b0dd92292dbec54dd09ac3f56d
Pedro Campos <pedro.campos@startaideia.com.br>
2021-03-05 18:12:34 -0300

    Implementacao Tela de Login

--- xlo_mobx/lib/components/custom_drawer/page_section.dart
+++ xlo_mobx/lib/components/custom_drawer/page_section.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:xlo_mobx/components/custom_drawer/page_tile.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 
 class PageSection extends StatelessWidget {
+  final PageStore pageStore = GetIt.I<PageStore>();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -9,42 +13,42 @@ class PageSection extends StatelessWidget {
         PageTile(
           label: 'Anúncios',
           iconData: Icons.list,
-          onTap: (){
-
+          onTap: () {
+            pageStore.setPage(0);
           },
-          highlighted: true
+          highlighted: pageStore.page == 0,
         ),
         PageTile(
           label: 'Inserir Anúncios',
           iconData: Icons.edit,
-          onTap: (){
-
+          onTap: () {
+            pageStore.setPage(1);
           },
-          highlighted: false
+          highlighted: pageStore.page == 1,
         ),
         PageTile(
           label: 'Chat',
           iconData: Icons.chat,
-          onTap: (){
-
+          onTap: () {
+            pageStore.setPage(2);
           },
-          highlighted: false
+          highlighted: pageStore.page == 2,
         ),
         PageTile(
           label: 'Favoritos',
           iconData: Icons.favorite,
-          onTap: (){
-
+          onTap: () {
+            pageStore.setPage(3);
           },
-          highlighted: false
+          highlighted: pageStore.page == 3,
         ),
         PageTile(
           label: 'Minha Conta',
           iconData: Icons.person,
-          onTap: (){
-
+          onTap: () {
+            pageStore.setPage(4);
           },
-          highlighted: false
+          highlighted: pageStore.page == 4,
         ),
       ],
     );
